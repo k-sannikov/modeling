febd2042bff54778ba604523a3b11329ae4c9ac6
Kirill Sannikov <kir.sannikow@mail.ru>
2023-09-28 19:51:52 +0300

    Поправил lw1

--- lw1/build.bat
+++ lw1/build.bat
@@ -2,8 +2,8 @@ docker build -t 2048_game .
 
 docker images
 
-docker create --name 2048_game_1 -P 2048_game
-docker create --name 2048_game_2 -P 2048_game
+docker create --name 2048_game_1 -p 1111:8080 2048_game
+docker create --name 2048_game_2 -p 2222:8080 2048_game
 
 docker start 2048_game_1
 docker start 2048_game_2
